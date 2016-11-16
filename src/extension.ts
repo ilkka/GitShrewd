703d218f1420c16f53c0f1d407935c4f7f35f412
Ilkka Laukkanen <ilkka@ilkka.io>
2016-11-16 16:56:24 +0200

    Properly manage keypress listener
    Dispose and get back so editing other docs still works

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { commands, CancellationToken, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, Uri, window, workspace } from 'vscode';
+import { commands, CancellationToken, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, TextEditor, Uri, window, workspace } from 'vscode';
 import * as Promise from 'bluebird';
 import * as simpleGit from 'simple-git/promise';
 
@@ -44,6 +44,8 @@ class CodeGitController {
     private subscriptions: Disposable[];
     private disposable: Disposable
     private contentProvider: CodeGitStatusContentProvider
+    private view: TextEditor;
+    private keypressDisposable: Disposable;
 
     constructor() {
         this.subscriptions = [];
@@ -56,9 +58,25 @@ class CodeGitController {
         this.disposable.dispose();
     }
 
-    private registerCommand(command: string, handler: (...args: any[]) => any) {
-        this.subscriptions.push(commands.registerCommand(command, handler, this));
+    /**
+     * Eventually dispose of the disposable (when the extension is deactivated).
+     * @param {Disposable} d the disposable.
+     * @return {Disposable} the disposable.
+     */
+    private eventuallyDispose(d: Disposable): Disposable {
+        this.subscriptions.push(d);
         this.disposable = Disposable.from(...this.subscriptions);
+        return d;
+    }
+
+    /**
+     * Register a handler for a command. The handler must be a member of this class.
+     * @param {string} command the command.
+     * @param {Function} handler the handler function.
+     * @return {Disposable} disposable for the registration.
+     */
+    private registerCommand(command: string, handler: (...args: any[]) => any): Disposable {
+        return this.eventuallyDispose(commands.registerCommand(command, handler, this));
     }
 
     private keyPress(what) {
@@ -69,10 +87,23 @@ class CodeGitController {
         workspace.openTextDocument(Uri.parse('codegit:status'))
             .then((document) => {
                 console.log('opened doc, showing editor');
+                this.eventuallyDispose(window.onDidChangeActiveTextEditor(
+                    (e: TextEditor) => {
+                        if (e !== this.view && this.keypressDisposable) {
+                            console.log('deactivate git kbd handler');
+                             this.keypressDisposable.dispose();
+                             this.keypressDisposable = null;
+                        } else if (e === this.view && !this.keypressDisposable) {
+                            console.log('activate git kbd handler');
+                            this.keypressDisposable = this.registerCommand('type', this.keyPress);
+                        }
+                    }
+                ));
                 window.showTextDocument(document)
                     .then((editor) => {
-                        console.log('editor displayed, registering key handler');
-                        this.registerCommand('type', this.keyPress);
+                        this.view = editor;
+                        console.log('git view displayed');
+                        this.keypressDisposable = this.registerCommand('type', this.keyPress);
                     });
             }, (err) => {
                 console.error(`failed to open doc: ${err}`);
