653d83655525aca357accdb6bf5fc894836377d0
Ilkka Laukkanen <ilkka@ilkka.io>
2016-11-17 15:54:37 +0200

    Write better comments

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,9 @@ import { commands, CancellationToken, Disposable, ExtensionContext, StatusBarAli
 import * as Promise from 'bluebird';
 import * as simpleGit from 'simple-git/promise';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+/**
+ * Called when the extension is activated.
+ */
 export function activate(context: ExtensionContext) {
     console.log('GitShrewd activating');
 
@@ -16,23 +17,61 @@ export function activate(context: ExtensionContext) {
     console.log('GitShrewd activated');
 }
 
+/**
+ * Deactivate handler, called when the extension is unloaded.
+ */
+export function deactivate() {
+    console.log('GitShrewd deactivating');
+    console.log('GitShrewd deactivated');
+}
+
+/**
+ * This class is responsible for generating all our git view content.
+ * The controller calls its methods, it announces to its listeners about content changes,
+ * the listeners call provideTextDocumentContent(), and it returns the content.
+ */
 class GitViewContentProvider {
     private changeListeners: ((u: Uri) => any)[];
 
+    /**
+     * Create a new instance of the content provider.
+     */
     constructor() {
         this.changeListeners = [];
     }
 
+    /**
+     * Provide text content for the given URI.
+     *
+     * @param {Uri} uri the URI for which the caller wants content.
+     * @param {CancellationToken} token a token with which the caller can cancel the operation.
+     *
+     * @return {string|Thenable<string>} content for the view.
+     */
     public provideTextDocumentContent(uri: Uri, token: CancellationToken): string | Thenable<string> {
         if (uri.path === 'status') {
             if (workspace.rootPath) {
-                return this.provideStatusContent();
+                return Promise.all([
+                    this.provideStatusContent()
+                ]).reduce((memo: string, val: string): string => memo += `${val}\n`, '');
             }
             return Promise.resolve('No folder open');
         }
         return Promise.reject(`unrecognized URI ${uri}`);
     }
 
+    /**
+     * Subscribe to content change events. Text editors showing documents
+     * that receive content from this provider call this. Returns a disposable.
+     *
+     * @param {Function} listener the callback function, called when content changed,
+     * with the URI of the changed view as parameter.
+     * @param {any} thisArg if given, context that should be bound for the listener when called.
+     * @param {Disposable[]} disposables optional list of disposables, where the disposable
+     * created by this function will be appended.
+     *
+     * @return {Disposable} a disposable which can be used to unsubscribe.
+     */
     public onDidChange(listener: (u: Uri) => any, thisArg?: any, disposables?: Disposable[]): Disposable {
         let that = thisArg || this;
         let func = listener.bind(that);
@@ -47,18 +86,31 @@ class GitViewContentProvider {
         return disposable;
     }
 
+    /**
+     * Request a git status view refresh.
+     */
     public refreshStatus() {
         console.log('status refresh requested');
         this.notifyListeners('gitshrewd:status');
     }
 
+    /**
+     * Notify listeners that the content of a given URI has changed.
+     *
+     * @param {Uri} uri the URI to pass to the listeners.
+     */
     private notifyListeners(uri: string) {
         for (let l of this.changeListeners) {
             l(Uri.parse(uri));
         }
     }
 
-    private provideStatusContent(): string | Thenable<string> {
+    /**
+     * Provide git status view content.
+     *
+     * @return {Thenable<string>} a string with the content of the status view.
+     */
+    private provideStatusContent(): Thenable<string> {
         const git = simpleGit(workspace.rootPath);
         return git.status()
         .then((stat) => {
@@ -89,6 +141,10 @@ class GitViewContentProvider {
     }
 }
 
+/**
+ * Git view controller. This class is responsible for creating and handling
+ * the editor view for interacting with the user.
+ */
 class GitViewController {
     private subscriptions: Disposable[];
     private disposable: Disposable
@@ -109,7 +165,9 @@ class GitViewController {
 
     /**
      * Eventually dispose of the disposable (when the extension is deactivated).
+     *
      * @param {Disposable} d the disposable.
+     *
      * @return {Disposable} the disposable.
      */
     private eventuallyDispose(d: Disposable): Disposable {
@@ -120,14 +178,21 @@ class GitViewController {
 
     /**
      * Register a handler for a command. The handler must be a member of this class.
+     *
      * @param {string} command the command.
      * @param {Function} handler the handler function.
+     *
      * @return {Disposable} disposable for the registration.
      */
     private registerCommand(command: string, handler: (...args: any[]) => any): Disposable {
         return this.eventuallyDispose(commands.registerCommand(command, handler, this));
     }
 
+    /**
+     * Keypress handler.
+     *
+     * @param {Object} what an event object with the property 'text' containing the keypress.
+     */
     private keyPress(what) {
         console.log(`KEYPRESS! ${what.text}`);
         if (what.text === 'r') {
@@ -135,6 +200,9 @@ class GitViewController {
         }
     }
 
+    /**
+     * Open a git status view.
+     */
     private openGitStatus() {
         workspace.openTextDocument(Uri.parse('gitshrewd:status'))
             .then((document) => {
@@ -165,9 +233,3 @@ class GitViewController {
             });
     }
 }
-
-export function deactivate() {
-    console.log('GitShrewd deactivating');
-    console.log('GitShrewd deactivated');
-}
-
